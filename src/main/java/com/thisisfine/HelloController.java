c347b07ead1d5d03762801d5500a11ef50cd963e
Roman Zhitkov <rzhitkov@gmail.com>
2020-10-01 14:37:18 +0300

    Upload files

---
+++ src/main/java/com/thisisfine/HelloController.java
@@ -0,0 +1,13 @@
+package com.thisisfine;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get("/{name}")
+    public String hello(String name) {
+        return "Hello, " + name;
+    }
+}
