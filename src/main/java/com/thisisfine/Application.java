1d4d98b31937919eb90fb353638911424f5f2aec
Roman <v-rzhitkov@ebsco.com>
2020-10-20 16:40:41 +0300

    Add swagger

--- src/main/java/com/thisisfine/Application.java
+++ src/main/java/com/thisisfine/Application.java
@@ -1,7 +1,16 @@
 package com.thisisfine;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Core Application",
+                version = "v0.1",
+                description = "API for core"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
